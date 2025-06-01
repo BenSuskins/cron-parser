6c53963f671dba53dc359055d55f1a60067b1de4
Ben Suskins <suskinsdevelopment@gmail.com>
2025-06-01 12:33:47 +0100

    Allow CLI running

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,3 +19,18 @@ tasks.test {
 kotlin {
     jvmToolchain(21)
 }
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "uk.co.suskins.MainKt"
+    }
+    // To avoid the duplicate handling strategy error
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    // To add all of the dependencies
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+}

44ff6fd67e9638a98cb58accf92daa45af20b532
Ben Suskins <suskinsdevelopment@gmail.com>
2025-06-01 16:51:48 +0100

    Fix indents

--- src/test/kotlin/CronParserTest.kt
+++ src/test/kotlin/CronParserTest.kt
@@ -12,12 +12,12 @@ class CronParserTest {
 
         assertEquals(
             """
-            minute 0 15 30 45
-            hour 0
-            day of month 1 15
-            month 1 2 3 4 5 6 7 8 9 10 11 12
-            day of week 1 2 3 4 5
-            command /usr/bin/find
+            minute         0 15 30 45
+            hour           0
+            day of month   1 15
+            month          1 2 3 4 5 6 7 8 9 10 11 12
+            day of week    1 2 3 4 5
+            command        /usr/bin/find
             """.trimIndent(),
             result
         )
@@ -27,60 +27,60 @@ class CronParserTest {
     fun `can parse minutes`() {
         assertEquals(
             """
-            minute 15
-            hour 0
-            day of month 1 15
-            month 1 2 3 4 5 6 7 8 9 10 11 12
-            day of week 1 2 3 4 5
-            command /usr/bin/find
+            minute         15
+            hour           0
+            day of month   1 15
+            month          1 2 3 4 5 6 7 8 9 10 11 12
+            day of week    1 2 3 4 5
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15 0 1,15 * 1-5 /usr/bin/find")
         )
 
         assertEquals(
             """
-            minute 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59
-            hour 0
-            day of month 1 15
-            month 1 2 3 4 5 6 7 8 9 10 11 12
-            day of week 1 2 3 4 5
-            command /usr/bin/find
+            minute         0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59
+            hour           0
+            day of month   1 15
+            month          1 2 3 4 5 6 7 8 9 10 11 12
+            day of week    1 2 3 4 5
+            command        /usr/bin/find
             """.trimIndent(),
             parse("* 0 1,15 * 1-5 /usr/bin/find")
         )
 
         assertEquals(
             """
-            minute 0 15 30 45
-            hour 0
-            day of month 1 15
-            month 1 2 3 4 5 6 7 8 9 10 11 12
-            day of week 1 2 3 4 5
-            command /usr/bin/find
+            minute         0 15 30 45
+            hour           0
+            day of month   1 15
+            month          1 2 3 4 5 6 7 8 9 10 11 12
+            day of week    1 2 3 4 5
+            command        /usr/bin/find
             """.trimIndent(),
             parse("*/15 0 1,15 * 1-5 /usr/bin/find")
         )
 
         assertEquals(
             """
-            minute 0 1 2 3 4 5 6 7 8 9 10 11 12
-            hour 0
-            day of month 1 15
-            month 1 2 3 4 5 6 7 8 9 10 11 12
-            day of week 1 2 3 4 5
-            command /usr/bin/find
+            minute         0 1 2 3 4 5 6 7 8 9 10 11 12
+            hour           0
+            day of month   1 15
+            month          1 2 3 4 5 6 7 8 9 10 11 12
+            day of week    1 2 3 4 5
+            command        /usr/bin/find
             """.trimIndent(),
             parse("0-12 0 1,15 * 1-5 /usr/bin/find")
         )
 
         assertEquals(
             """
-            minute 0 12
-            hour 0
-            day of month 1 15
-            month 1 2 3 4 5 6 7 8 9 10 11 12
-            day of week 1 2 3 4 5
-            command /usr/bin/find
+            minute         0 12
+            hour           0
+            day of month   1 15
+            month          1 2 3 4 5 6 7 8 9 10 11 12
+            day of week    1 2 3 4 5
+            command        /usr/bin/find
             """.trimIndent(),
             parse("0,12 0 1,15 * 1-5 /usr/bin/find")
         )
@@ -102,60 +102,60 @@ class CronParserTest {
     fun `can parse hours`() {
         assertEquals(
             """
-            minute 15
-            hour 0
-            day of month 1 15
-            month 1 2 3 4 5 6 7 8 9 10 11 12
-            day of week 1 2 3 4 5
-            command /usr/bin/find
+            minute         15
+            hour           0
+            day of month   1 15
+            month          1 2 3 4 5 6 7 8 9 10 11 12
+            day of week    1 2 3 4 5
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15 0 1,15 * 1-5 /usr/bin/find")
         )
 
         assertEquals(
             """
-            minute 15
-            hour 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
-            day of month 1 15
-            month 1 2 3 4 5 6 7 8 9 10 11 12
-            day of week 1 2 3 4 5
-            command /usr/bin/find
+            minute         15
+            hour           0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
+            day of month   1 15
+            month          1 2 3 4 5 6 7 8 9 10 11 12
+            day of week    1 2 3 4 5
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15 * 1,15 * 1-5 /usr/bin/find")
         )
 
         assertEquals(
             """
-            minute 15
-            hour 0 12
-            day of month 1 15
-            month 1 2 3 4 5 6 7 8 9 10 11 12
-            day of week 1 2 3 4 5
-            command /usr/bin/find
+            minute         15
+            hour           0 12
+            day of month   1 15
+            month          1 2 3 4 5 6 7 8 9 10 11 12
+            day of week    1 2 3 4 5
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15 */12 1,15 * 1-5 /usr/bin/find")
         )
 
         assertEquals(
             """
-            minute 15
-            hour 0 1 2 3 4 5 6 7 8 9 10 11 12
-            day of month 1 15
-            month 1 2 3 4 5 6 7 8 9 10 11 12
-            day of week 1 2 3 4 5
-            command /usr/bin/find
+            minute         15
+            hour           0 1 2 3 4 5 6 7 8 9 10 11 12
+            day of month   1 15
+            month          1 2 3 4 5 6 7 8 9 10 11 12
+            day of week    1 2 3 4 5
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15 0-12 1,15 * 1-5 /usr/bin/find")
         )
 
         assertEquals(
             """
-            minute 15
-            hour 0 12
-            day of month 1 15
-            month 1 2 3 4 5 6 7 8 9 10 11 12
-            day of week 1 2 3 4 5
-            command /usr/bin/find
+            minute         15
+            hour           0 12
+            day of month   1 15
+            month          1 2 3 4 5 6 7 8 9 10 11 12
+            day of week    1 2 3 4 5
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15 0,12 1,15 * 1-5 /usr/bin/find")
         )
@@ -177,60 +177,60 @@ class CronParserTest {
     fun `can parse days`() {
         assertEquals(
             """
-            minute 15
-            hour 0
-            day of month 1
-            month 1 2 3 4 5 6 7 8 9 10 11 12
-            day of week 1 2 3 4 5
-            command /usr/bin/find
+            minute         15
+            hour           0
+            day of month   1
+            month          1 2 3 4 5 6 7 8 9 10 11 12
+            day of week    1 2 3 4 5
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15 0 1 * 1-5 /usr/bin/find")
         )
 
         assertEquals(
             """
-            minute 15
-            hour 0
-            day of month 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
-            month 1 2 3 4 5 6 7 8 9 10 11 12
-            day of week 1 2 3 4 5
-            command /usr/bin/find
+            minute         15
+            hour           0
+            day of month   1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
+            month          1 2 3 4 5 6 7 8 9 10 11 12
+            day of week    1 2 3 4 5
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15 0 * * 1-5 /usr/bin/find")
         )
 
         assertEquals(
             """
-            minute 15
-            hour 0
-            day of month 1 16 31
-            month 1 2 3 4 5 6 7 8 9 10 11 12
-            day of week 1 2 3 4 5
-            command /usr/bin/find
+            minute         15
+            hour           0
+            day of month   1 16 31
+            month          1 2 3 4 5 6 7 8 9 10 11 12
+            day of week    1 2 3 4 5
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15 0 */15 * 1-5 /usr/bin/find")
         )
 
         assertEquals(
             """
-            minute 15
-            hour 0
-            day of month 1 2 3 4 5 6 7 8 9 10 11 12
-            month 1 2 3 4 5 6 7 8 9 10 11 12
-            day of week 1 2 3 4 5
-            command /usr/bin/find
+            minute         15
+            hour           0
+            day of month   1 2 3 4 5 6 7 8 9 10 11 12
+            month          1 2 3 4 5 6 7 8 9 10 11 12
+            day of week    1 2 3 4 5
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15 0 1-12 * 1-5 /usr/bin/find")
         )
 
         assertEquals(
             """
-            minute 15
-            hour 0
-            day of month 1 15
-            month 1 2 3 4 5 6 7 8 9 10 11 12
-            day of week 1 2 3 4 5
-            command /usr/bin/find
+            minute         15
+            hour           0
+            day of month   1 15
+            month          1 2 3 4 5 6 7 8 9 10 11 12
+            day of week    1 2 3 4 5
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15 0 1,15 * 1-5 /usr/bin/find")
         )
@@ -252,60 +252,60 @@ class CronParserTest {
     fun `can parse months`() {
         assertEquals(
             """
-            minute 15
-            hour 0
-            day of month 1
-            month 1
-            day of week 1 2 3 4 5
-            command /usr/bin/find
+            minute         15
+            hour           0
+            day of month   1
+            month          1
+            day of week    1 2 3 4 5
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15 0 1 1 1-5 /usr/bin/find")
         )
 
         assertEquals(
             """
-            minute 15
-            hour 0
-            day of month 1
-            month 1 2 3 4 5 6 7 8 9 10 11 12
-            day of week 1 2 3 4 5
-            command /usr/bin/find
+            minute         15
+            hour           0
+            day of month   1
+            month          1 2 3 4 5 6 7 8 9 10 11 12
+            day of week    1 2 3 4 5
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15 0 1 * 1-5 /usr/bin/find")
         )
 
         assertEquals(
             """
-            minute 15
-            hour 0
-            day of month 1
-            month 1 7
-            day of week 1 2 3 4 5
-            command /usr/bin/find
+            minute         15
+            hour           0
+            day of month   1
+            month          1 7
+            day of week    1 2 3 4 5
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15 0 1 */6 1-5 /usr/bin/find")
         )
 
         assertEquals(
             """
-            minute 15
-            hour 0
-            day of month 1
-            month 1 2 3 4 5 6
-            day of week 1 2 3 4 5
-            command /usr/bin/find
+            minute         15
+            hour           0
+            day of month   1
+            month          1 2 3 4 5 6
+            day of week    1 2 3 4 5
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15 0 1 1-6 1-5 /usr/bin/find")
         )
 
         assertEquals(
             """
-            minute 15
-            hour 0
-            day of month 1
-            month 1 6
-            day of week 1 2 3 4 5
-            command /usr/bin/find
+            minute         15
+            hour           0
+            day of month   1
+            month          1 6
+            day of week    1 2 3 4 5
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15 0 1 1,6 1-5 /usr/bin/find")
         )
@@ -327,60 +327,60 @@ class CronParserTest {
     fun `can parse day of week`() {
         assertEquals(
             """
-            minute 15
-            hour 0
-            day of month 1
-            month 1
-            day of week 1
-            command /usr/bin/find
+            minute         15
+            hour           0
+            day of month   1
+            month          1
+            day of week    1
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15 0 1 1 1 /usr/bin/find")
         )
 
         assertEquals(
             """
-            minute 15
-            hour 0
-            day of month 1
-            month 1
-            day of week 1 2 3 4 5 6 7
-            command /usr/bin/find
+            minute         15
+            hour           0
+            day of month   1
+            month          1
+            day of week    1 2 3 4 5 6 7
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15 0 1 1 * /usr/bin/find")
         )
 
         assertEquals(
             """
-            minute 15
-            hour 0
-            day of month 1
-            month 1
-            day of week 1 4 7
-            command /usr/bin/find
+            minute         15
+            hour           0
+            day of month   1
+            month          1
+            day of week    1 4 7
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15 0 1 1 */3 /usr/bin/find")
         )
 
         assertEquals(
             """
-            minute 15
-            hour 0
-            day of month 1
-            month 1
-            day of week 1 2 3 4 5
-            command /usr/bin/find
+            minute         15
+            hour           0
+            day of month   1
+            month          1
+            day of week    1 2 3 4 5
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15 0 1 1 1-5 /usr/bin/find")
         )
 
         assertEquals(
             """
-            minute 15
-            hour 0
-            day of month 1
-            month 1
-            day of week 1 6
-            command /usr/bin/find
+            minute         15
+            hour           0
+            day of month   1
+            month          1
+            day of week    1 6
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15 0 1 1 1,6 /usr/bin/find")
         )
@@ -402,36 +402,36 @@ class CronParserTest {
     fun `can parse command`() {
         assertEquals(
             """
-            minute 15
-            hour 0
-            day of month 1
-            month 1
-            day of week 1
-            command /usr/bin/find
+            minute         15
+            hour           0
+            day of month   1
+            month          1
+            day of week    1
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15 0 1 1 1 /usr/bin/find")
         )
 
         assertEquals(
             """
-            minute 15
-            hour 0
-            day of month 1
-            month 1
-            day of week 1
-            command hello world
+            minute         15
+            hour           0
+            day of month   1
+            month          1
+            day of week    1
+            command        hello world
             """.trimIndent(),
             parse("15 0 1 1 1 hello world")
         )
 
         assertEquals(
             """
-            minute 15
-            hour 0
-            day of month 1
-            month 1
-            day of week 1
-            command /hello/world
+            minute         15
+            hour           0
+            day of month   1
+            month          1
+            day of week    1
+            command        /hello/world
             """.trimIndent(),
             parse("15 0 1 1 1 /hello/world")
         )
@@ -441,36 +441,36 @@ class CronParserTest {
     fun `can parse edge cases`() {
         assertEquals(
             """
-            minute 15 16 17 18
-            hour 0
-            day of month 1
-            month 1
-            day of week 1
-            command /usr/bin/find
+            minute         15 16 17 18
+            hour           0
+            day of month   1
+            month          1
+            day of week    1
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15,16,17,18 0 1 1 1 /usr/bin/find")
         )
 
         assertEquals(
             """
-            minute 15 16 17 18
-            hour 0
-            day of month 1
-            month 1
-            day of week 1
-            command /usr/bin/find
+            minute         15 16 17 18
+            hour           0
+            day of month   1
+            month          1
+            day of week    1
+            command        /usr/bin/find
             """.trimIndent(),
             parse("   15,16,17,18 0 1 1 1 /usr/bin/find    ")
         )
 
         assertEquals(
             """
-            minute 15 16 17 18
-            hour 0
-            day of month 1
-            month 1
-            day of week 1
-            command /usr/bin/find
+            minute         15 16 17 18
+            hour           0
+            day of month   1
+            month          1
+            day of week    1
+            command        /usr/bin/find
             """.trimIndent(),
             parse("15,15,16,17,18 0 1 1 1 /usr/bin/find")
         )

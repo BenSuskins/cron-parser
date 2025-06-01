35220d41be2f08ea2ac5a383fe98fcd7031f6b98
Ben Suskins <suskinsdevelopment@gmail.com>
2025-06-01 12:41:01 +0100

    Split out production ccode

---
+++ src/main/kotlin/CronParser.kt
@@ -0,0 +1,12 @@
+package uk.co.suskins
+
+fun parse(input: String): String {
+    return """
+            minute 0 15 30 45
+            hour 0
+            day of month 1 15
+            month 1 2 3 4 5 6 7 8 9 10 11 12
+            day of week 1 2 3 4 5
+            command /usr/bin/find
+            """.trimIndent()
+}
--- src/test/kotlin/CronParserTest.kt
+++ src/test/kotlin/CronParserTest.kt
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test
+import uk.co.suskins.parse
 import kotlin.test.assertEquals
 
 class CronParserTest {
@@ -19,16 +20,5 @@ class CronParserTest {
             """.trimIndent(),
             result
         )
-    }
-
-    private fun parse(input: String): String {
-        return """
-            minute 0 15 30 45
-            hour 0
-            day of month 1 15
-            month 1 2 3 4 5 6 7 8 9 10 11 12
-            day of week 1 2 3 4 5
-            command /usr/bin/find
-            """.trimIndent()
     }
 }

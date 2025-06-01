045c349c4b17d82b36f7461f8216b143a254e959
Ben Suskins <suskinsdevelopment@gmail.com>
2025-06-01 12:40:27 +0100

    Initial test case

---
+++ src/test/kotlin/CronParserTest.kt
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class CronParserTest {
+    @Test
+    fun `can parse an input`() {
+        val input = "*/15 0 1,15 * 1-5 /usr/bin/find"
+
+        val result = parse(input)
+
+        assertEquals(
+            """
+            minute 0 15 30 45
+            hour 0
+            day of month 1 15
+            month 1 2 3 4 5 6 7 8 9 10 11 12
+            day of week 1 2 3 4 5
+            command /usr/bin/find
+            """.trimIndent(),
+            result
+        )
+    }
+
+    private fun parse(input: String): String {
+        return """
+            minute 0 15 30 45
+            hour 0
+            day of month 1 15
+            month 1 2 3 4 5 6 7 8 9 10 11 12
+            day of week 1 2 3 4 5
+            command /usr/bin/find
+            """.trimIndent()
+    }
+}

662ce0f700c75cdd0e2e1a71ef1012528cc397d2
Ben Suskins <suskinsdevelopment@gmail.com>
2025-06-01 12:50:16 +0100

    Parse minutes naively

--- src/main/kotlin/CronParser.kt
+++ src/main/kotlin/CronParser.kt
@@ -1,12 +1,30 @@
 package uk.co.suskins
 
-fun parse(input: String): String {
+fun parse(inputString: String): String {
+    val input = splitInput(inputString)
+
     return """
-            minute 0 15 30 45
+            minute ${input.parseMinutes()}
             hour 0
             day of month 1 15
             month 1 2 3 4 5 6 7 8 9 10 11 12
             day of week 1 2 3 4 5
             command /usr/bin/find
             """.trimIndent()
+}
+
+fun splitInput(inputString: String): Input {
+    val split = inputString.split(" ")
+
+    return Input(split[0])
+}
+
+class Input(private val minute: String) {
+    fun parseMinutes(): String {
+        return if (minute.contains("*")) {
+            "0 15 30 45"
+        } else {
+            minute
+        }
+    }
 }
--- src/test/kotlin/CronParserTest.kt
+++ src/test/kotlin/CronParserTest.kt
@@ -1,9 +1,11 @@
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import uk.co.suskins.parse
 import kotlin.test.assertEquals
 
 class CronParserTest {
     @Test
+    @Disabled
     fun `can parse an input`() {
         val input = "*/15 0 1,15 * 1-5 /usr/bin/find"
 
@@ -19,6 +21,33 @@ class CronParserTest {
             command /usr/bin/find
             """.trimIndent(),
             result
+        )
+    }
+
+    @Test
+    fun `can parse minutes`() {
+        assertEquals(
+            """
+            minute 15
+            hour 0
+            day of month 1 15
+            month 1 2 3 4 5 6 7 8 9 10 11 12
+            day of week 1 2 3 4 5
+            command /usr/bin/find
+            """.trimIndent(),
+            parse("15 0 1,15 * 1-5 /usr/bin/find")
+        )
+
+        assertEquals(
+            """
+            minute 0 15 30 45
+            hour 0
+            day of month 1 15
+            month 1 2 3 4 5 6 7 8 9 10 11 12
+            day of week 1 2 3 4 5
+            command /usr/bin/find
+            """.trimIndent(),
+            parse("*/15 0 1,15 * 1-5 /usr/bin/find")
         )
     }
 }
